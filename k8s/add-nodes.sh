bf0fa63470e5ce08963700e812d5ccc4fe436c34
jshiner-accenture <john.shiner@accenture.com>
2020-03-31 09:01:26 -0500

    scale web nodes script

--- k8s/add-nodes.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-kubectl apply -f ./flask-container-service.yml
-
-date >> log.txt
-echo 'added web nodes' >> log.txt
-kubectl get pods >> log.txt
--- k8s/rmv-nodes.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-kubectl apply -f ./flask-container-service.yml
-
-date >> log.txt
-echo 'removed web nodes' >> log.txt
-kubectl get pods >> log.txt
